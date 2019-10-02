24933cc682da5c8c3ef69a680f48d15d3f7864aa
ggichure <g.gichure@outlook.com>
2019-10-02 20:13:19 +0300

    migrated to androidX
    migrated to androidX and  updated dep

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\firefox\flutter_windows_v1.5.4-hotfix.2-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\pr\giffy_dialog\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=E:\firefox\flutter_windows_v1.5.4-hotfix.2-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
