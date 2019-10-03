68819efb2a015db7853d4a30abd34ace7ab04650
Madhukesh <madhukesh04@gmail.com>
2019-10-03 15:56:02 +0530

    Added landscape orientation support for dialog

--- lib/src/asset.dart
+++ lib/src/asset.dart
@@ -32,79 +32,163 @@ class AssetGiffyDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(cornerRadius)),
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.6,
-        width: MediaQuery.of(context).size.width * 0.8,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: (MediaQuery.of(context).size.height / 2) * 0.6,
-                  child: Card(
-                    elevation: 0.0,
-                    margin: EdgeInsets.all(0.0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(cornerRadius),
-                            topLeft: Radius.circular(cornerRadius))),
-                    clipBehavior: Clip.antiAlias,
-                    child: image,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: title,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: description,
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: !onlyOkButton
-                    ? MainAxisAlignment.spaceEvenly
-                    : MainAxisAlignment.center,
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(cornerRadius)),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.6,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  !onlyOkButton
-                      ? RaisedButton(
-                          color: buttonCancelColor ?? Colors.grey,
-                          shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.circular(buttonRadius)),
-                          onPressed: () => Navigator.of(context).pop(),
-                          child: buttonCancelText ??
-                              Text(
-                                'Cancel',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                        )
-                      : Container(),
-                  RaisedButton(
-                    color: buttonOkColor ?? Colors.green,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(buttonRadius)),
-                    onPressed: onOkButtonPressed ?? () {},
-                    child: buttonOkText ??
-                        Text(
-                          'OK',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: (MediaQuery.of(context).size.height / 2) * 0.6,
+                    child: Card(
+                      elevation: 0.0,
+                      margin: EdgeInsets.all(0.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(cornerRadius),
+                              topLeft: Radius.circular(cornerRadius))),
+                      clipBehavior: Clip.antiAlias,
+                      child: image,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: title,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: description,
                   ),
                 ],
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: !onlyOkButton
+                      ? MainAxisAlignment.spaceEvenly
+                      : MainAxisAlignment.center,
+                  children: <Widget>[
+                    !onlyOkButton
+                        ? RaisedButton(
+                            color: buttonCancelColor ?? Colors.grey,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.circular(buttonRadius)),
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: buttonCancelText ??
+                                Text(
+                                  'Cancel',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                          )
+                        : Container(),
+                    RaisedButton(
+                      color: buttonOkColor ?? Colors.green,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(buttonRadius)),
+                      onPressed: onOkButtonPressed ?? () {},
+                      child: buttonOkText ??
+                          Text(
+                            'OK',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(cornerRadius)),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.7,
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: (MediaQuery.of(context).size.height) * 0.7,
+                    child: Card(
+                      elevation: 0.0,
+                      margin: EdgeInsets.all(0.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(cornerRadius),
+                              topLeft: Radius.circular(cornerRadius))),
+                      clipBehavior: Clip.antiAlias,
+                      child: image,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          title,
+                          description,
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: !onlyOkButton
+                            ? MainAxisAlignment.spaceEvenly
+                            : MainAxisAlignment.center,
+                        children: <Widget>[
+                          !onlyOkButton
+                              ? RaisedButton(
+                                  color: buttonCancelColor ?? Colors.grey,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(buttonRadius)),
+                                  onPressed: () => Navigator.of(context).pop(),
+                                  child: buttonCancelText ??
+                                      Text(
+                                        'Cancel',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                )
+                              : Container(),
+                          RaisedButton(
+                            color: buttonOkColor ?? Colors.green,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.circular(buttonRadius)),
+                            onPressed: onOkButtonPressed ?? () {},
+                            child: buttonOkText ??
+                                Text(
+                                  'OK',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
