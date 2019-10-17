965d24aa42f240c6763514b84d0b0cb3467eb9a3
Sahil Kumar <xdsahil@gmail.com>
2019-10-18 01:57:25 +0530

    Add widget test for landscape mode.
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- test/giffy_dialog_test.dart
+++ test/giffy_dialog_test.dart
@@ -1,12 +1,52 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../example/lib/main.dart';
 import 'package:image_test_utils/image_test_utils.dart';
 
 void main() {
-  testWidgets('Check button smoke test', (WidgetTester tester) async {
+  testWidgets('Check Portrait Dialog test', (WidgetTester tester) async {
     provideMockedNetworkImages(() async {
       // Build our app and trigger a frame.
       await tester.pumpWidget(new MyApp());
+
+      expect(find.byKey(keys[0]), findsOneWidget);
+
+      await tester.tap(find.byKey(keys[0]));
+      await tester.pump();
+
+      expect(find.byKey(keys[1]), findsOneWidget);
+
+      await tester.tap(find.text('Cancel'));
+      await tester.pump();
+
+      expect(find.byKey(keys[2]), findsOneWidget);
+
+      await tester.tap(find.byKey(keys[2]));
+      await tester.pump();
+
+      expect(find.byKey(keys[3]), findsOneWidget);
+
+      await tester.tap(find.text('Cancel'));
+      await tester.pump();
+
+      expect(find.byKey(keys[4]), findsOneWidget);
+
+      await tester.tap(find.byKey(keys[4]));
+      await tester.pump();
+
+      expect(find.byKey(keys[5]), findsOneWidget);
+    });
+  });
+
+  testWidgets('Check Landscape Dialog test', (WidgetTester tester) async {
+    provideMockedNetworkImages(() async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(new MyApp());
+
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeRight,
+        DeviceOrientation.landscapeLeft,
+      ]);
 
       expect(find.byKey(keys[0]), findsOneWidget);
 
