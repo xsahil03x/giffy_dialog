87ce2e44161d892beb35b1b4472de0ea09f06a28
Sahil Kumar <xdsahil@gmail.com>
2019-10-18 15:32:04 +0530

    Add widget test for portrait mode. (#29)
    * Add widget test for landscape mode.
    
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>
    
    * Mock orientation for both the tests
    
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>
    
    * Assert flareAnimation property for null value
    
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>
    
    * Add assertion tests for all dialogs
    
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>
    
    * Fix orientation bug in test
    
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- test/giffy_dialog_test.dart
+++ test/giffy_dialog_test.dart
@@ -1,39 +1,138 @@
+import 'dart:ui';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:giffy_dialog/giffy_dialog.dart';
 import '../example/lib/main.dart';
 import 'package:image_test_utils/image_test_utils.dart';
 
+const double PORTRAIT_WIDTH = 1800.0;
+const double PORTRAIT_HEIGHT = 2400.0;
+const double LANDSCAPE_WIDTH = 2400.0;
+const double LANDSCAPE_HEIGHT = 1800.0;
+
 void main() {
-  testWidgets('Check button smoke test', (WidgetTester tester) async {
-    provideMockedNetworkImages(() async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(new MyApp());
+  group('Dialog Assertion Test', () {
+    testWidgets(
+        'AssetGiffyDialog throws if initialized with null image and title',
+            (WidgetTester tester) async {
+          try {
+            await tester.pumpWidget(AssetGiffyDialog(
+              onOkButtonPressed: () {},
+              image: null,
+              title: null,
+            ));
+          } catch (error) {
+            expect(error, isAssertionError);
+          }
+        });
 
-      expect(find.byKey(keys[0]), findsOneWidget);
+    testWidgets(
+        'FlareGiffyDialog throws if initialized with null flarePath, flareAnimation and title',
+            (WidgetTester tester) async {
+          try {
+            await tester.pumpWidget(FlareGiffyDialog(
+              onOkButtonPressed: () {},
+              flarePath: null,
+              title: null,
+              flareAnimation: null,
+            ));
+          } catch (error) {
+            expect(error, isAssertionError);
+          }
+        });
 
-      await tester.tap(find.byKey(keys[0]));
-      await tester.pump();
+    testWidgets(
+        'NetworkGiffyDialog throws if initialized with null image and title',
+            (WidgetTester tester) async {
+          try {
+            await tester.pumpWidget(NetworkGiffyDialog(
+              onOkButtonPressed: () {},
+              image: null,
+              title: null,
+            ));
+          } catch (error) {
+            expect(error, isAssertionError);
+          }
+        });
+  });
 
-      expect(find.byKey(keys[1]), findsOneWidget);
+  group('Dialog Smoke and Orientation Test', () {
+    testWidgets('Check Portrait Dialog test', (WidgetTester tester) async {
+      provideMockedNetworkImages(() async {
+        final TestWidgetsFlutterBinding binding =
+        TestWidgetsFlutterBinding.ensureInitialized();
+        binding.window.physicalSizeTestValue =
+        (Size(PORTRAIT_WIDTH, PORTRAIT_HEIGHT));
 
-      await tester.tap(find.text('Cancel'));
-      await tester.pump();
+        // Build our app and trigger a frame.
+        await tester.pumpWidget(new MyApp());
 
-      expect(find.byKey(keys[2]), findsOneWidget);
+        expect(find.byKey(keys[0]), findsOneWidget);
 
-      await tester.tap(find.byKey(keys[2]));
-      await tester.pump();
+        await tester.tap(find.byKey(keys[0]));
+        await tester.pump();
 
-      expect(find.byKey(keys[3]), findsOneWidget);
+        expect(find.byKey(keys[1]), findsOneWidget);
 
-      await tester.tap(find.text('Cancel'));
-      await tester.pump();
+        await tester.tap(find.text('Cancel'));
+        await tester.pump();
 
-      expect(find.byKey(keys[4]), findsOneWidget);
+        expect(find.byKey(keys[2]), findsOneWidget);
 
-      await tester.tap(find.byKey(keys[4]));
-      await tester.pump();
+        await tester.tap(find.byKey(keys[2]));
+        await tester.pump();
 
-      expect(find.byKey(keys[5]), findsOneWidget);
+        expect(find.byKey(keys[3]), findsOneWidget);
+
+        await tester.tap(find.text('Cancel'));
+        await tester.pump();
+
+        expect(find.byKey(keys[4]), findsOneWidget);
+
+        await tester.tap(find.byKey(keys[4]));
+        await tester.pump();
+
+        expect(find.byKey(keys[5]), findsOneWidget);
+      });
+    });
+
+    testWidgets('Check Landscape Dialog test', (WidgetTester tester) async {
+      provideMockedNetworkImages(() async {
+        final TestWidgetsFlutterBinding binding =
+        TestWidgetsFlutterBinding.ensureInitialized();
+        binding.window.physicalSizeTestValue =
+            Size(LANDSCAPE_WIDTH, LANDSCAPE_HEIGHT);
+
+        // Build our app and trigger a frame.
+        await tester.pumpWidget(new MyApp());
+
+        expect(find.byKey(keys[0]), findsOneWidget);
+
+        await tester.tap(find.byKey(keys[0]));
+        await tester.pump();
+
+        expect(find.byKey(keys[1]), findsOneWidget);
+
+        await tester.tap(find.text('Cancel'));
+        await tester.pump();
+
+        expect(find.byKey(keys[2]), findsOneWidget);
+
+        await tester.tap(find.byKey(keys[2]));
+        await tester.pump();
+
+        expect(find.byKey(keys[3]), findsOneWidget);
+
+        await tester.tap(find.text('Cancel'));
+        await tester.pump();
+
+        expect(find.byKey(keys[4]), findsOneWidget);
+
+        await tester.tap(find.byKey(keys[4]));
+        await tester.pump();
+
+        expect(find.byKey(keys[5]), findsOneWidget);
+      });
     });
   });
 }
