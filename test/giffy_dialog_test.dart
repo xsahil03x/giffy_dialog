9e1350707bef0b86203fb670919e384d5abe71cb
Sahil Kumar <xdsahil@gmail.com>
2019-10-18 15:23:23 +0530

    Fix orientation bug in test
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- test/giffy_dialog_test.dart
+++ test/giffy_dialog_test.dart
@@ -5,63 +5,64 @@ import 'package:giffy_dialog/giffy_dialog.dart';
 import '../example/lib/main.dart';
 import 'package:image_test_utils/image_test_utils.dart';
 
-const double PORTRAIT_WIDTH = 600.0;
-const double PORTRAIT_HEIGHT = 800.0;
-const double LANDSCAPE_WIDTH = 800.0;
-const double LANDSCAPE_HEIGHT = 600.0;
+const double PORTRAIT_WIDTH = 1800.0;
+const double PORTRAIT_HEIGHT = 2400.0;
+const double LANDSCAPE_WIDTH = 2400.0;
+const double LANDSCAPE_HEIGHT = 1800.0;
 
 void main() {
   group('Dialog Assertion Test', () {
     testWidgets(
         'AssetGiffyDialog throws if initialized with null image and title',
-        (WidgetTester tester) async {
-      try {
-        await tester.pumpWidget(AssetGiffyDialog(
-          onOkButtonPressed: () {},
-          image: null,
-          title: null,
-        ));
-      } catch (error) {
-        expect(error, isAssertionError);
-      }
-    });
+            (WidgetTester tester) async {
+          try {
+            await tester.pumpWidget(AssetGiffyDialog(
+              onOkButtonPressed: () {},
+              image: null,
+              title: null,
+            ));
+          } catch (error) {
+            expect(error, isAssertionError);
+          }
+        });
 
     testWidgets(
         'FlareGiffyDialog throws if initialized with null flarePath, flareAnimation and title',
-        (WidgetTester tester) async {
-      try {
-        await tester.pumpWidget(FlareGiffyDialog(
-          onOkButtonPressed: () {},
-          flarePath: null,
-          title: null,
-          flareAnimation: null,
-        ));
-      } catch (error) {
-        expect(error, isAssertionError);
-      }
-    });
+            (WidgetTester tester) async {
+          try {
+            await tester.pumpWidget(FlareGiffyDialog(
+              onOkButtonPressed: () {},
+              flarePath: null,
+              title: null,
+              flareAnimation: null,
+            ));
+          } catch (error) {
+            expect(error, isAssertionError);
+          }
+        });
 
     testWidgets(
         'NetworkGiffyDialog throws if initialized with null image and title',
-        (WidgetTester tester) async {
-      try {
-        await tester.pumpWidget(NetworkGiffyDialog(
-          onOkButtonPressed: () {},
-          image: null,
-          title: null,
-        ));
-      } catch (error) {
-        expect(error, isAssertionError);
-      }
-    });
+            (WidgetTester tester) async {
+          try {
+            await tester.pumpWidget(NetworkGiffyDialog(
+              onOkButtonPressed: () {},
+              image: null,
+              title: null,
+            ));
+          } catch (error) {
+            expect(error, isAssertionError);
+          }
+        });
   });
 
   group('Dialog Smoke and Orientation Test', () {
     testWidgets('Check Portrait Dialog test', (WidgetTester tester) async {
       provideMockedNetworkImages(() async {
         final TestWidgetsFlutterBinding binding =
-            TestWidgetsFlutterBinding.ensureInitialized();
-        await binding.setSurfaceSize(Size(PORTRAIT_WIDTH, PORTRAIT_HEIGHT));
+        TestWidgetsFlutterBinding.ensureInitialized();
+        binding.window.physicalSizeTestValue =
+        (Size(PORTRAIT_WIDTH, PORTRAIT_HEIGHT));
 
         // Build our app and trigger a frame.
         await tester.pumpWidget(new MyApp());
@@ -98,8 +99,9 @@ void main() {
     testWidgets('Check Landscape Dialog test', (WidgetTester tester) async {
       provideMockedNetworkImages(() async {
         final TestWidgetsFlutterBinding binding =
-            TestWidgetsFlutterBinding.ensureInitialized();
-        await binding.setSurfaceSize(Size(LANDSCAPE_WIDTH, LANDSCAPE_HEIGHT));
+        TestWidgetsFlutterBinding.ensureInitialized();
+        binding.window.physicalSizeTestValue =
+            Size(LANDSCAPE_WIDTH, LANDSCAPE_HEIGHT);
 
         // Build our app and trigger a frame.
         await tester.pumpWidget(new MyApp());
