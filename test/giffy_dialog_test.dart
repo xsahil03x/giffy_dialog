ead9b98a5741f5a25954ba4ea8c40a1b43fd5494
Sahil Kumar <xdsahil@gmail.com>
2019-10-22 04:47:21 +0530

    Update test according to new entry animation feature
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- test/giffy_dialog_test.dart
+++ test/giffy_dialog_test.dart
@@ -127,27 +127,27 @@ void main() {
         expect(find.byKey(keys[0]), findsOneWidget);
 
         await tester.tap(find.byKey(keys[0]));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[1]), findsOneWidget);
 
         await tester.tap(find.text('Cancel'));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[2]), findsOneWidget);
 
         await tester.tap(find.byKey(keys[2]));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[3]), findsOneWidget);
 
         await tester.tap(find.text('Cancel'));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[4]), findsOneWidget);
 
         await tester.tap(find.byKey(keys[4]));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[5]), findsOneWidget);
       });
@@ -166,27 +166,27 @@ void main() {
         expect(find.byKey(keys[0]), findsOneWidget);
 
         await tester.tap(find.byKey(keys[0]));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[1]), findsOneWidget);
 
         await tester.tap(find.text('Cancel'));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[2]), findsOneWidget);
 
         await tester.tap(find.byKey(keys[2]));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[3]), findsOneWidget);
 
         await tester.tap(find.text('Cancel'));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[4]), findsOneWidget);
 
         await tester.tap(find.byKey(keys[4]));
-        await tester.pump();
+        await tester.pumpAndSettle();
 
         expect(find.byKey(keys[5]), findsOneWidget);
       });
