3c3ec7bec13470a1720721c3bd30d9bb81983fa9
Kawaeee <kawaekc@gmail.com>
2019-10-24 09:42:22 +0700

    add animation variation

--- lib/src/base_dialog.dart
+++ lib/src/base_dialog.dart
@@ -1,11 +1,17 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 enum EntryAnimation {
-  DEFAULT,
-  LEFT_RIGHT,
-  RIGHT_LEFT,
-  TOP_BOTTOM,
-  BOTTOM_TOP,
+  DEFAULT, // Center
+  LEFT,
+  RIGHT,
+  TOP,
+  BOTTOM,
+  TOP_LEFT,
+  TOP_RIGHT,
+  BOTTOM_LEFT,
+  BOTTOM_RIGHT,
 }
 
 class BaseGiffyDialog extends StatefulWidget {
@@ -45,27 +51,29 @@ class BaseGiffyDialog extends StatefulWidget {
 class _BaseGiffyDialogState extends State<BaseGiffyDialog>
     with TickerProviderStateMixin {
   AnimationController _animationController;
-  Animation _entryAnimation;
+  Animation<Offset> _entryAnimation;
 
   get _start {
     switch (widget.entryAnimation) {
       case EntryAnimation.DEFAULT:
         break;
-      case EntryAnimation.LEFT_RIGHT:
-      case EntryAnimation.TOP_BOTTOM:
-        return -1.0;
-      case EntryAnimation.RIGHT_LEFT:
-      case EntryAnimation.BOTTOM_TOP:
-        return 1.0;
+      case EntryAnimation.TOP:
+        return Offset(0.0, -1.0);
+      case EntryAnimation.TOP_LEFT:
+        return Offset(-1.0, -1.0);
+      case EntryAnimation.TOP_RIGHT:
+        return Offset(1.0, -1.0);
+      case EntryAnimation.LEFT:
+        return Offset(-1.0, 0.0);
+      case EntryAnimation.RIGHT:
+        return Offset(1.0, 0.0);
+      case EntryAnimation.BOTTOM:
+        return Offset(0.0, 1.0);
+      case EntryAnimation.BOTTOM_LEFT:
+        return Offset(-1.0, 1.0);
+      case EntryAnimation.BOTTOM_RIGHT:
+        return Offset(1.0, 1.0);
     }
-  }
-
-  get _animationAxis {
-    if (widget.entryAnimation == EntryAnimation.TOP_BOTTOM ||
-        widget.entryAnimation == EntryAnimation.BOTTOM_TOP)
-      return 1;
-    else
-      return 0;
   }
 
   get _isDefaultEntryAnimation =>
@@ -79,7 +87,8 @@ class _BaseGiffyDialogState extends State<BaseGiffyDialog>
         vsync: this,
         duration: Duration(milliseconds: 300),
       );
-      _entryAnimation = Tween(begin: _start, end: 0.0).animate(
+      _entryAnimation =
+          Tween<Offset>(begin: _start, end: Offset(0.0, 0.0)).animate(
         CurvedAnimation(
           parent: _animationController,
           curve: Curves.easeIn,
@@ -149,7 +158,7 @@ class _BaseGiffyDialogState extends State<BaseGiffyDialog>
                 child: widget.title,
               ),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(3.0),
                 child: widget.description,
               ),
               _buildButtonsBar(context),
@@ -199,7 +208,6 @@ class _BaseGiffyDialogState extends State<BaseGiffyDialog>
 
   @override
   Widget build(BuildContext context) {
-    final animationAxis = _animationAxis;
     final width = MediaQuery.of(context).size.width;
     final isPortrait =
         MediaQuery.of(context).orientation == Orientation.portrait;
@@ -209,8 +217,8 @@ class _BaseGiffyDialogState extends State<BaseGiffyDialog>
       child: Container(
         transform: !_isDefaultEntryAnimation
             ? Matrix4.translationValues(
-                animationAxis == 0 ? _entryAnimation.value * width : 0,
-                animationAxis == 1 ? _entryAnimation.value * width : 0,
+                _entryAnimation.value.dx * width,
+                _entryAnimation.value.dy * width,
                 0,
               )
             : null,
